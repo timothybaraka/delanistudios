230b1e2bcd61be3c8832b9940911373756ce6ac5
timothybaraka <timothybaraka44@gmail.com>
2019-12-08 13:49:09 +0300

    Add the final images

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $("#img1").click(function(){
+        $("text1").toggle();
+    });
+});
+
+$("").click(function(){
+    $("").toggle();
+});
+});
+
+$("").click(function(){
+    $("").toggle();
+});
+});
+
+$("").click(function(){
+    $("").toggle();
+});
+});
