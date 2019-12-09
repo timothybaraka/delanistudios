03b7537630449aa3d39a4a87ba37eead001e1e11
timothybaraka <timothybaraka44@gmail.com>
2019-12-09 17:35:07 +0300

    Add toggle  to the icons

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,50 @@ $(document).ready(function(){
     $("#subscribe").click(function(){
         prompt("we have received your message thank you for reaching out to us");
     });
-});
 
-$("").click(function(){
-    $("").toggle();
-});
-});
 
-$("").click(function(){
-    $("").toggle();
-});
-});
+$("#icon1").on("click", function(){
+    $("#des").hide();
+    $("#icon1").toggle();
+    $("#des").show();
+    });
+$("#des").on("click",function(){
+    $("#icon1").hide();
+    $("#des").toggle();
+    $("#icon1").show();
+    });
 
-$("").click(function(){
-    $("").toggle();
-});
+
+    $("#icon2").on("click", function(){
+        $("#dev").hide();
+        $("#icon2").toggle();
+        $("#dev").show();
+        });
+    $("#dev").on("click",function(){
+        $("#icon2").hide();
+        $("#dev").toggle();
+        $("#icon2").show();
+        });    
+
+
+        $("#icon3").on("click", function(){
+            $("#prod").hide();
+            $("#icon3").toggle();
+            $("#prod").show();
+            });
+        $("#prod").on("click",function(){
+            $("#icon3").hide();
+            $("#prod").toggle();
+            $("#icon3").show();
+            });        
+// $("#icon2").click(function(){
+//     $("#dev").show();
+//     $("icon2").hide();
+// });
+
+
+// $("#icon3").click(function(){
+//     $("#prod").show();
+//     $("icon3").hide();
+// });
 });
